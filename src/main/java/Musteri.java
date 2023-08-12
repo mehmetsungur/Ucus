0f54132d9737b3cf4acb6ba0e2f83c69ac6e6d88
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-12 20:28:57 +0300

    update

--- src/main/java/Musteri.java
+++ src/main/java/Musteri.java
@@ -1,5 +1,63 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Musteri {
     private String isim;
-    private String musteriNo;
+    private String musteriId;
+    private static int musteriPassword = 3000;
+    private static int id = 1000;
+    private static int flightNo=9000;
 
+    Scanner scan = new Scanner(System.in);
+    HashMap<String,String> isimId = new HashMap<String,String>();
+    HashMap<String,Integer> isimPwd = new HashMap<String,Integer>();
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getMusteriId() {
+        return musteriId;
+    }
+
+    public void setMusteriId(String musteriId) {
+        this.musteriId = musteriId;
+    }
+
+    public static int getMusteriPassword() {
+        return musteriPassword;
+    }
+
+    public static void setMusteriPassword(int musteriPassword) {
+        Musteri.musteriPassword = musteriPassword;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Musteri.id = id;
+    }
+
+    public static int getFlightNo() {
+        return flightNo;
+    }
+
+    public static void setFlightNo(int flightNo) {
+        Musteri.flightNo = flightNo;
+    }
+
+    public void musteriEkle(){
+        isimId.put(getIsim(), getMusteriId());
+        isimPwd.put(getIsim(), getMusteriPassword());
+    }
+
+    public void signIn(){
+
+    }
 }
